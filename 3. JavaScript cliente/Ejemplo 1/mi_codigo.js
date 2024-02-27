1f26fae054b94e413d82b1ad19958ec244ddfe2b
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2024-02-27 00:58:39 +0000

    Create files to demo the use of JavaScript

--- 3. JavaScript cliente/Ejemplo 1/mi_codigo.js
+++ 3. JavaScript cliente/Ejemplo 1/mi_codigo.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const mi_boton = document.getElementById('mi_buton');
 const mi_lista = document.getElementById('mi_lista');
 mi_boton.addEventListener('click', presionar_boton);
@@ -11,4 +12,20 @@ function presionar_boton() {
   mi_lista.innerHTML += '<li>Una opción ' + contador + '</li>';
   // mi_lista.style.color = colores[contador % colores.length];
   mi_lista.style['color'] = colores[contador % colores.length];
+=======
+const mi_buton = document.getElementById("mi_boton");
+const mi_titulo = document.getElementById("mi_titulo");
+const mi_buton_rojo = document.getElementById("mi_boton_rojo");
+const cuerpo = document.getElementById("cuerpo");
+
+mi_buton.addEventListener("click", boton_presionado);
+mi_buton_rojo.addEventListener("click", enrojece);
+
+function boton_presionado() {
+  mi_titulo.innerText = "Presionaste el botón";
+}
+
+function enrojece() {
+  cuerpo.classList.add("rojo");
+>>>>>>> a82413d (Create files to demo the use of JavaScript)
 }
